e38b41b0bdcc236db4c34c91f1aa8acf05ebecee
Deepika-r01 <149507785+Deepika-r01@users.noreply.github.com>
2024-04-08 10:46:44 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -4,6 +4,6 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo 'Welcome to Instance' >> /var/www/html/index.html
+echo 'Welcome to Instance1' >> /var/www/html/index.html
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
